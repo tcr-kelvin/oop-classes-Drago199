9ce6522f0aa6dc81d638118d1b422bd82cfde063
Drago199 <32331796+Drago199@users.noreply.github.com>
2019-03-06 13:17:41 +0100

    Add files via upload

---
+++ Opdracht 3/Opdracht3HTML.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<html>
+
+<form action="Opdracht3.php" method="post">
+    prijs1: <input type="text" name="prijs1"> <br/>
+    prijs2: <input type="text" name="prijs2"> <br/>
+    prijs3: <input type="text" name="prijs3"> <br/>
+    <input type="submit">
+</form>
+
+</html>
